833ef8a644e0f5487ec621d1373e531e9b7b6bb5
YSChan15 <fl5447rn@go.minnstate.edu>
2024-03-20 11:57:16 -0500

    Add Arduino Files

---
+++ Arduino_Measuring_System/README.md
@@ -0,0 +1,133 @@
+# Arduino Nano code 
+
+## Introduction 
+* This project is part of the senior design of the following title:
+
+**Facial Recognition and Detection using Augmented Reality Headset with Cloud Server and SmartMesh IP Network**
+
+* Advisor: Dr Zheng 
+
+* Commitee: Dr Hossain, Dr Cavalcanti
+  
+* Group members: Yu Sheng Chan, Brycen Hillukka, Hayden Scott, Brandon Wieberdink 
+
+> This project is sponsored by a grant from the Emerson cooperation. 
+
+
+> Authors:
+> - Brycen Hillukka (Spring 2023)
+> - Yu Sheng Chan   (Spring 2023)
+>
+> Reads the measured distance from the SF11/C LiDAR sensor using an 
+> Arduino Nano using the I2C protocol. Connect the SCL connection from 
+> the SF11/C to pin A5 on the Arduino, and the SDA connection to pin A4.
+> 
+> The code for reading from the SF11/C and storing the value on the Arduino
+> were given in the datasheet for the LiDAR sensor. Refer to Github Datasheet section (SF11 datasheet)
+>
+> Further modifications were made by:
+> - Yu Sheng Chan   (Fall 2023)
+> 
+>  New addition: GPS and Electronic Compass module 
+> 
+>  Reads the GPS location and Electronic Compass moudle reading to determine the coordinates of a measured point 
+>  Math is done on the AR Headset / UWP side instead of Arduino for quicker transfer rate 
+> 
+>  The Electronic Compass module can be changed to either QMC or HMC chip. 
+>  Please follow the datasheet for specific pinout / voltage requirements of the chip. 
+>  If neither chip is used, please use your own library. 
+>  Refer to README.MD files attached 
+> 
+>  Use a high gain antenna if GPS disconnects frequently or could not connect during outdoor use. 
+>  Please note that the GPS is intended for outdoor use. It is expected to have no signal indoors. 
+>  Please refer to the specifications of the datasheet of GPS for antenna compatibality. 
+> 
+
+## Before you start 
+
+### Program / hardware needed 
+
+- Arduino IDE 
+>Install Arduino IDE from this [link](https://downloads.arduino.cc/arduino-1.8.19-windows.exe).
+
+> **NOTE** : This code is programmed using Arduino IDE 1.8.19, which is a Legacy IDE.
+
+- Mini-B USB cable 
+
+### Libraries
+
+#### Library of DFROBOT_QMC5883
+* Download the included zip libraries or go to [DFRobot Link](https://github.com/DFRobot/DFRobot_QMC5883) to download the zip folder
+
+* To include the libraries, go to Sketch -> Include Library -> Add ZIP Library and add your downloaded library
+
+* Use #include <DFRobot_QMC5883.h> as your header file
+
+
+#### Library of MPU6050_light
+* To include the libraries, go to Sketch -> Include Library -> Manage Library
+
+* Search for [MPU6050_light](https://github.com/rfetick/MPU6050_light) by rfetick and click install
+
+* Use #include <MPU6050_light.h> as your header file
+
+
+#### Library of GT-U7 GPS module
+* To include the libraries, go to Sketch -> Include Library -> Manage Library
+
+* Search for [TinyGPSPlus](https://github.com/mikalhart/TinyGPSPlus) by Mikah Hart and click install
+
+* Use #include <TinyGPS++.h> as your header file
+
+
+**NOTE FOR GPS Module Connection**
+
+> * To get a good and stable GPS connection, it is best to use the system outdoors / close to window.
+>
+> * Use your OWN location. Find it via [Magnetic Declination Website](http://magnetic-declination.com/) to get your inclination or declination angle.
+> 
+> * Replace your angle at the following line:
+>
+>  **For example:**
+>
+>  **float declinationAngle = (X + (Y / 60.0)) / (180 / PI);**
+>
+>  **St Cloud / Waite Park Angle = 0'38E**
+>
+>  
+>  ***Replace X and Y with the following angle above***
+>
+>  
+>  **float declinationAngle = (0 + (38.0 / 60.0)) / (180 / PI);**
+
+## Programming / Debugging the Arduino Nano
+
+* Remove the BLE module first before you program the Arduino Nano.
+> If Arduino is not being reprogrammed, there's no need to remove the BLE module.
+
+* If Arduino errors out during the upload, check if the board used is correct. Make sure the board selected is correct (In this case its Arduino Nano).
+> Change the processor to the one with "old bootloader" in its name if it does not program.  
+
+* Make sure the lights on GPS, MPU6050 gyroscopic sensor, and LiDAR module are getting powered (indicated by the LED's).
+
+* Calibrate the compass sensor by turning the entire system 360 degrees. 
+
+* Use the Serial Monitor to observe the data being collected. If data(distance) measured is not consistent when being left idle, reseat the LiDAR cable.
+
+
+## Purchase link 
+Purchase link can be found below if replacement part is needed. 
+
+**Please note that I am not affiliated with any of the listed product, and the link is provided solely for your convenience.**
+
+[**GPS SENSOR**](https://www.amazon.com/Navigation-Satellite-Compatible-Microcontroller-Geekstory/dp/B07PRGBLX7?th=1)
+
+[**BLE module**](https://www.amazon.com/DSD-TECH-Bluetooth-iBeacon-Arduino/dp/B06WGZB2N4)
+
+[**MPU 6050**](https://www.amazon.com/HiLetgo-MPU-6050-Accelerometer-Gyroscope-Converter/dp/B01DK83ZYQ/ref=asc_df_B01DK83ZYQ/?tag=hyprod-20&linkCode=df0&hvadid=642109977814&hvpos=&hvnetw=g&hvrand=8430803264096386904&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=1020086&hvtargid=pla-1260970162741&gclid=EAIaIQobChMI7fH7s9OPggMV_1J_AB3O2gLQEAQYASABEgKCrPD_BwE&th=1)
+
+[**HMC 5883**](https://amazon.com/HiLetgo-GY-271-QMC5883L-Compass-Magnetometer/dp/B008V9S64E/ref=asc_df_B008V9S64E/?tag=hyprod-20&linkCode=df0&hvadid=241933244562&hvpos=&hvnetw=g&hvrand=4729607995161709394&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=1020086&hvtargid=pla-650173234261&psc=1)
+
+[**Arduino NANO Microcontroller**](https://store.arduino.cc/products/arduino-nano)
+
+[**SF11/C LiDAR Sensor**](https://www.mouser.com/ProductDetail/LightWare-LiDAR/SF11-C?qs=iLbezkQI%252BsjRfbQfbtoV7g%3D%3D&mgh=1&gad_source=1&gclid=EAIaIQobChMIi9Sq6PL3gwMVoVdHAR0QQQRgEAQYAiABEgKM2_D_BwE)
---
+++ Arduino_Measuring_System/arduino_AR_headset/arduino_AR_headset.ino
@@ -0,0 +1,237 @@
+/*
+ * Reads the measured distance from the SF11/C LiDAR sensor using an 
+ * Arduino Nano using the I2C protocol. Connect the SCL connection from 
+ * the SF11/C to pin A5 on the Arduino, and the SDA connection to pin A4.
+ * 
+ * The code for reading from the SF11/C and storing the value on the Arduino
+ * were given in the datasheet for the LiDAR sensor. Refer to Github Datasheet section (SF11 datasheet)
+ * 
+ * Modifications were made by:
+ *  - Brycen Hillukka (Spring 2023) 
+ *  - Yu Sheng Chan   (Spring 2023)
+ * 
+ * 
+ * Further modifications were made by:
+ *  - Yu Sheng Chan   (Fall 2023)
+ *  
+ *  
+ * New addition: GPS and Electronic Compass module 
+ * 
+ * Reads the GPS location and Electronic Compass moudle reading to determine the coordinates of a measured point 
+ * Math is done on the AR Headset / UWP side instead of Arduino for quicker transfer rate 
+ * 
+ * The Electronic Compass module can be changed to either QMC or HMC chip. 
+ * Please follow the datasheet for specific pinout / voltage requirements of the chip. 
+ * If neither chip is used, please use your own library. 
+ * Refer to README.MD files attached 
+ * 
+ * Use a high gain antenna if GPS disconnects frequently or could not connect during outdoor use. 
+ * Please note that the GPS is intended for outdoor use. It is expected to have no signal indoors. 
+ * Please refer to the specifications of the datasheet of GPS for antenna compatibality. 
+ */
+
+// Include necessary libraries 
+#include <Wire.h>
+#include <MPU6050_light.h>
+#include <math.h>
+#include <SoftwareSerial.h>
+#include <TinyGPS++.h>
+#include <DFRobot_QMC5883.h>
+
+// Creates an mpu object
+MPU6050 mpu(Wire);                      
+
+//Using pin 2 and 3 as software serial pin
+int RXPin = 2;
+int TXPin = 3;
+SoftwareSerial gpsSerial(RXPin,TXPin);
+
+//Creats gps object
+TinyGPSPlus gps;
+
+//Creates compass object 
+DFRobot_QMC5883 compass;
+
+float distance;                         // Distance will be in meters
+byte a, b;                              // Bytes read by the I2C port
+
+
+void setup() {
+  Serial.begin(9600);                   // Ensure serial monitor set to this value also
+  gpsSerial.begin(9600);                // Start serial monitor of gps 
+  Wire.begin();                         // Creates a wire object
+
+  //Infinite while loop to check if compass is present
+  while (!compass.begin())
+  {
+    Serial.println("Could not find a valid QMC5883 sensor, check wiring!");
+    delay(500);
+  }
+
+  //2 Models of compass, will initialize to whichever model it detects
+  //Change the Range,Measurements,DataRate and Sampling rate according to datasheet / library file 
+  if(compass.isHMC())
+  {
+        Serial.println("Initialize HMC5883");
+        compass.setRange(HMC5883L_RANGE_1_3GA);
+        compass.setMeasurementMode(HMC5883L_CONTINOUS);
+        compass.setDataRate(HMC5883L_DATARATE_15HZ);
+        compass.setSamples(HMC5883L_SAMPLES_8);
+  }
+  else if(compass.isQMC())
+  {
+        Serial.println("Initialize QMC5883");
+        compass.setRange(QMC5883_RANGE_2GA);
+        compass.setMeasurementMode(QMC5883_CONTINOUS); 
+        compass.setDataRate(QMC5883_DATARATE_200HZ);
+        compass.setSamples(QMC5883_SAMPLES_8);
+  }
+  
+  mpu.begin();                          // Creates connection for writing to the MPU6050
+  Serial.println(F("Calculating gyro offset, do not move MPU6050"));
+  delay(1000);
+  mpu.calcGyroOffsets();                // This does the calibration for MPU6050
+  Wire.endTransmission(true);           // Ends transmission with MPU6050
+  
+  Wire.beginTransmission(0x66);         // Enable transmission to the SF11C
+  Wire.write(0);                        // Write the distance location -> 0
+  Wire.endTransmission(true);           // End the transmission
+}
+ 
+void loop() 
+{
+  //When GPS is ready to be read, display the data
+  while(gpsSerial.available() > 0)
+  {
+    //Read the data from GPS
+    if(gps.encode(gpsSerial.read()))
+    {
+      //Display the data / send it via Bluetooth 
+      displayInfo();
+    }
+  }
+
+  // If 5000 milliseconds pass and there are no characters coming in
+  // over the software serial port, show a "No GPS detected" error
+  
+  if (millis() > 5000 && gps.charsProcessed() < 10)
+  {
+    Serial.println("No GPS detected");
+    //while(true);
+  } 
+  
+  Vector norm = compass.readNormalize();
+
+  // Calculate heading
+  float heading = atan2(norm.YAxis, norm.XAxis);
+
+  // Set declination angle on your location and fix heading
+  // You can find your declination on: http://magnetic-declination.com/
+  // (+) Positive or (-) for negative
+  // For St Cloud / Waite Park Angle declination angle is 4'26E (positive)
+  // Formula: (deg + (min / 60.0)) / (180 / M_PI);
+  float declinationAngle = (0 + (38.0 / 60.0)) / (180 / PI);
+  heading += declinationAngle;
+
+  // Correct for heading < 0deg and heading > 360deg
+  if (heading < 0){
+    heading += 2 * PI;
+  }
+
+  if (heading > 2 * PI){
+    heading -= 2 * PI;
+  }
+
+  // Convert to degrees
+  float headingDegrees = heading * 180/M_PI; 
+
+  // Output
+  //Serial.print(" Heading = ");
+  //Serial.print(heading);
+  //Serial.print(" Degress = ");
+  Serial.print(headingDegrees);
+  Serial.println("AN");
+
+  delay(50);
+  
+  Wire.beginTransmission(0x66);         // Begin transmission with SF11C
+  Wire.write(0);                        // 
+  Wire.endTransmission();               // End transmission for writing to the SF11C
+  Wire.requestFrom(0x66, 2);            // Request 2 bytes from the SF11
+  
+  while(Wire.available())               // Make sure the SF11 sends the required number of bytes
+  {
+    a = Wire.read();                    // Fetch the high byte of the distance
+    b = Wire.read();                    // Fetch the low byte of the distance
+  }
+
+  distance = (float)(a * 256 + b)/100;  // Convert to a floating point value in meters
+  
+  Serial.print(distance);               // Print the distance to the serial port
+  Serial.println("W");
+  
+  delay(50);
+
+  mpu.begin();                          // Creates connection for writing to the MPU6050
+  Wire.write(0);                        //
+  Wire.endTransmission();               // Ends transmission with MPU6050
+  mpu.update();                         // 
+  
+  int phi = mpu.getAngleY();            // Measure the angle between x-y plane and point
+  int theta = mpu.getAngleZ() + 90;     // Measure the angle between x axis and point
+  
+  // Get the z distance
+  float distanceZ = distance * sin(phi * PI / 180);
+
+  // Get the x-y plane distance
+  float distanceXY = distance * cos(phi * PI / 180);
+
+  // Get the y distance
+  float distanceY = distanceXY * sin(theta * PI / 180);
+
+  // Get the z distance
+  float distanceX = distanceXY * cos(theta * PI / 180);
+
+  // Send distance X with payload "X" as marker  
+  Serial.print(distanceX);
+  Serial.println("X");
+  
+  delay(50);
+
+  // Send distance Y with payload "Y" as marker
+  Serial.print(distanceY);
+  Serial.println("Y");
+  
+  delay(50);
+
+  // Send distance Z with payload "Z" as marker 
+  Serial.print(distanceZ);
+  Serial.println("Z");
+  
+  delay(50);
+}
+
+// Function to display information via the GPS 
+void displayInfo()
+{
+  if (gps.location.isValid())
+  {
+    // Send latitude with payload "LA" as marker
+    Serial.print(gps.location.lat(), 6);
+    Serial.println("LA");
+    
+    delay(50);
+    
+    // Send longitude with payload "LO" as marker
+    Serial.print(gps.location.lng(), 6);
+    Serial.println("LO");
+    
+    delay(50);
+
+    // Send altitude with payload "AL" as marker
+    Serial.print(gps.altitude.meters());
+    Serial.println("AL");
+    
+    delay(50);
+  }
+}
